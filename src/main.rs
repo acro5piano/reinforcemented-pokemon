d2561ab411bbdbb853b8f073b9dd1152334e67f9
Kay Gosho <ketsume0211@gmail.com>
2022-07-30 23:12:44 +0900

    debug mode

--- src/main.rs
+++ src/main.rs
@@ -19,17 +19,23 @@ fn main() {
 
     let mut q: Q = HashMap::new();
 
-    for step in 0..1000000 {
+    for step in 0..10000 {
+        println!("-----------------------------");
+        dbg!(&step);
+
         let s_t = agent.state.clone();
         let action = agent.state.pick_action();
+        dbg!(&action);
 
         agent.take_action(&action);
+        dbg!(&agent.state);
 
         let s_t_next = agent.state.clone();
         let r_t_next = agent.state.reward();
 
         let v_t = {
             let old_value = q.get(&s_t).and_then(|m| m.get(&action));
+            dbg!(&old_value);
             let new_action_values = q.get(&s_t_next);
             let max_next = new_action_values
                 .and_then(|m| m.values().max_by(|a, b| a.partial_cmp(b).unwrap()))
@@ -38,10 +44,16 @@ fn main() {
                 x + alpha * (r_t_next + gamma * max_next - x)
             })
         };
+        dbg!(&v_t);
 
         q.entry(s_t)
             .or_insert_with(HashMap::new)
             .insert(action, v_t);
+
+        dbg!(&q);
+        println!("-----------------------------");
+
+        stdin().read(&mut [0]).unwrap();
     }
     dbg!(&q);
 }
