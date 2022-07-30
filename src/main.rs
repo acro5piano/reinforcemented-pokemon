c4b99b70a112c7ae41ff382a3092201165ca9723
Kay Gosho <ketsume0211@gmail.com>
2022-07-30 23:37:57 +0900

    implement first q learning

--- src/main.rs
+++ src/main.rs
@@ -20,40 +20,41 @@ fn main() {
     let mut q: Q = HashMap::new();
 
     for step in 0..10000 {
-        println!("-----------------------------");
-        dbg!(&step);
+        // dbg!("-----------------------------");
+        // dbg!(&step);
 
         let s_t = agent.state.clone();
         let action = agent.state.pick_action();
-        dbg!(&action);
+        // dbg!(&action);
 
         agent.take_action(&action);
-        dbg!(&agent.state);
+        // dbg!(&agent.state);
 
         let s_t_next = agent.state.clone();
         let r_t_next = agent.state.reward();
 
         let v_t = {
-            let old_value = q.get(&s_t).and_then(|m| m.get(&action));
-            dbg!(&old_value);
-            let new_action_values = q.get(&s_t_next);
-            let max_next = new_action_values
+            let old_value = q
+                .get(&s_t)
+                .and_then(|m| m.get(&action))
+                .unwrap_or(&INITIAL_VALUE);
+            // dbg!(&old_value);
+            let max_next = q
+                .get(&s_t_next)
                 .and_then(|m| m.values().max_by(|a, b| a.partial_cmp(b).unwrap()))
                 .unwrap_or(&INITIAL_VALUE);
-            old_value.map_or(INITIAL_VALUE, |x| {
-                x + alpha * (r_t_next + gamma * max_next - x)
-            })
+            (1.0 - alpha) * (old_value) + alpha * (r_t_next + gamma * max_next)
         };
-        dbg!(&v_t);
+        // dbg!(&v_t);
 
         q.entry(s_t)
             .or_insert_with(HashMap::new)
             .insert(action, v_t);
 
-        dbg!(&q);
-        println!("-----------------------------");
+        // dbg!(&q);
+        // dbg!("-----------------------------");
 
-        stdin().read(&mut [0]).unwrap();
+        // stdin().read(&mut [0]).unwrap();
     }
     dbg!(&q);
 }
